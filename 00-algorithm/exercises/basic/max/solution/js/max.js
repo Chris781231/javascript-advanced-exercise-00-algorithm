7164a29d06fa37d29b6356734051efbfb1ab3071
Chris781231 <ligeti.karoly78@gmail.com>
2024-01-14 17:42:30 +0100

    Add basic solutions.

--- 00-algorithm/exercises/basic/max/solution/js/max.js
+++ 00-algorithm/exercises/basic/max/solution/js/max.js
@@ -1,1 +1,8 @@
+const getTheLargestElement = (arr) => {
+  if (arr.length === 0) return undefined;
+  let max = arr[0];
+  arr.forEach((item) => (item > max ? (max = item) : null));
+  return max;
+};
+
 export default getTheLargestElement;
--- 00-algorithm/exercises/basic/min/solution/js/min.js
+++ 00-algorithm/exercises/basic/min/solution/js/min.js
@@ -1,1 +1,9 @@
+const getTheSmallestElement = (arr) => {
+  if (arr.length === 0) return undefined;
+
+  let min = arr[0];
+  arr.forEach((item) => (item < min ? (min = item) : null));
+  return min;
+};
+
 export default getTheSmallestElement;
